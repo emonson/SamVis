65fb22d5a1506df8768a60532ca0dcebd4061996
Eric E Monson <emonson@cs.duke.edu>
2013-03-26 15:18:59 -0400

    got ellipses data from server working

--- ellipses.php
+++ ellipses.php
@@ -1,3 +1,4 @@
 <?php
-print_r(file_get_contents("http://emo2.trinity.duke.edu:9000/"));
+$n_pts = $_GET['n'];
+print_r(file_get_contents("http://emo2.trinity.duke.edu:9000/newdata?n=" . $n_pts));
 ?>
