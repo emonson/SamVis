48831f1f85ba26de266fcb4d42934439aad26f5d
Eric E Monson <emonson@cs.duke.edu>
2013-05-10 14:23:18 -0400

    two column layout with labeled proj basis node. still work to do on coloring

--- d3_ellipse_tree_brush.js
+++ d3_ellipse_tree_brush.js
@@ -12,7 +12,9 @@ var background_ellipse_ids = [];
 var all_ellipse_data = [];
 var all_ellipse_bounds = [];
 
+// NOTE: These are both here for ellipses and in CSS for rectangles...
 var selectColor = "gold";
+var basisColor = "black";
 var ellipseStrokeWidth = 2;
 var cScale = d3.scale.linear()
 						.domain([0.0, 0.5, 1.0])
@@ -24,7 +26,7 @@ var w_el = 400;
 var h_el = 400;
 var padding = 40;
 // Initial selection. node_id sets the scale for now...
-var node_id = 50;
+var node_id = 0;
 var basis_id = 0;
 
 // Ellipse plot scale functions with placeholder domains
@@ -64,7 +66,7 @@ svg.append("g")
 
 // - - - - - - - - - - - - - - - -
 // Icicle view variables
-var w_ice = 500;
+var w_ice = 420;
 var h_ice = 300;
 var x_ice = d3.scale.linear().range([0, w_ice]);
 var y_ice = d3.scale.linear().range([0, h_ice]);
@@ -141,77 +143,89 @@ var getReprojectedEllipsesFromServer = function() {
 	});
 };
 
-var updateEllipses = function() {
+var setEllipseStrokeColor = function(d,i) {
+	// Selected overrides basis
+	if (d[5] == node_id) { return selectColor; }
+	if (d[5] == basis_id) { return basisColor; }
+	else { return cScale(scalardata[d[5]]);}
+};
 
-		// Regenerate visible ellipse data from two lists of ids
-		// For now tag on extra flag identifying class of ellipse (background, visible)
-		var visible_ellipse_data = [];
-		for (var ii = 0; ii < background_ellipse_ids.length; ii++) {
-			var tmp_data = all_ellipse_data[background_ellipse_ids[ii]].slice();
-			tmp_data.push('background');
-			visible_ellipse_data.push(tmp_data);
-		}
-		for (var ii = 0; ii < visible_ellipse_ids.length; ii++) {
-			var tmp_data = all_ellipse_data[visible_ellipse_ids[ii]].slice();
-			tmp_data.push('visible');
-			visible_ellipse_data.push(tmp_data);
-		}
+var setEllipseStrokeWidth = function(d,i) {
+	// Selected ellipse coloring (stroke) overrides background (no stroke)
+	if (d[5] == node_id) { return ellipseStrokeWidth; }
+	if (d[5] == basis_id) { return ellipseStrokeWidth; }
+	if (d[6] == 'background') {return 0;}
+	else {return ellipseStrokeWidth;}
+};
 
-		// Update the ellipses
-		var els = svg.selectAll("ellipse")
-				.data(visible_ellipse_data, function(d){return d[5];});
-		
-		els.transition()
-				.duration(500)		
-				.attr("transform", function(d){return "translate(" + xScale(d[0]) + "," + yScale(d[1]) + ")  rotate(" + d[4] + ")";})
-				.attr("stroke", function(d,i){return d[5] == node_id ? selectColor : cScale(scalardata[d[5]]);})
-				.attr("stroke-width", function(d) {
-						// Selected overrides background
-						if (d[5] == node_id) { return ellipseStrokeWidth; }
-						if (d[6] == 'background') {return 0;}
-						else {return ellipseStrokeWidth;}
-				 })
-				.attr("rx", function(d) { return xrScale(d[2]); })
-				.attr("ry", function(d) { return yrScale(d[3]); });
+var updateEllipses = function(trans_dur) {
+	
+	// Default value for transition duration
+	trans_dur = trans_dur || 500;
+	
+	// Regenerate visible ellipse data from two lists of ids
+	// For now tag on extra flag identifying class of ellipse (background, visible)
+	var visible_ellipse_data = [];
+	for (var ii = 0; ii < background_ellipse_ids.length; ii++) {
+		var tmp_data = all_ellipse_data[background_ellipse_ids[ii]].slice();
+		tmp_data.push('background');
+		visible_ellipse_data.push(tmp_data);
+	}
+	for (var ii = 0; ii < visible_ellipse_ids.length; ii++) {
+		var tmp_data = all_ellipse_data[visible_ellipse_ids[ii]].slice();
+		tmp_data.push('visible');
+		visible_ellipse_data.push(tmp_data);
+	}
 
-		
-		els.enter()
-				.append("ellipse")
-				.attr("id", function(d) {return "e_" + d[5];})
-				.attr("stroke", function(d) {return d[5] == node_id ? selectColor : cScale(scalardata[d[5]]);})
-				.attr("stroke-width", function(d) {
-						// Selected overrides background
-						if (d[5] == node_id) { return ellipseStrokeWidth; }
-						if (d[6] == 'background') {return 0;}
-						else {return ellipseStrokeWidth;}
-				 })
-				.attr("transform", function(d){return "translate(" + xScale(d[0]) + "," + yScale(d[1]) + ")  rotate(" + d[4] + ")";})
-				.attr("rx", function(d) { return xrScale(d[2]); })
-				.attr("ry", function(d) { return yrScale(d[3]); })
-				.on("click", clickfctn)
-				.on("dblclick", dblclickfctn);
-		
-		els.exit()
-				.remove();
-		
-		updateAxes();
+	// Update the ellipses
+	var els = svg.selectAll("ellipse")
+			.data(visible_ellipse_data, function(d){return d[5];});
+	
+	els.transition()
+			.duration(trans_dur)		
+			.attr("transform", function(d){return "translate(" + xScale(d[0]) + "," + yScale(d[1]) + ")  rotate(" + d[4] + ")";})
+			.attr("stroke", setEllipseStrokeColor )
+			.attr("stroke-width", setEllipseStrokeWidth )
+			.attr("rx", function(d) { return xrScale(d[2]); })
+			.attr("ry", function(d) { return yrScale(d[3]); });
+	
+	els.enter()
+			.append("ellipse")
+			.attr("id", function(d) {return "e_" + d[5];})
+			.attr("stroke", setEllipseStrokeColor )
+			.attr("stroke-width", setEllipseStrokeWidth )
+			.attr("transform", function(d){return "translate(" + xScale(d[0]) + "," + yScale(d[1]) + ")  rotate(" + d[4] + ")";})
+			.attr("rx", function(d) { return xrScale(d[2]); })
+			.attr("ry", function(d) { return yrScale(d[3]); })
+			.on("click", clickfctn)
+			.on("dblclick", dblclickfctn);
+	
+	els.exit()
+			.remove();
+	
+	updateAxes();
 };
 
 // Ellipse click function (update projection basis)
 var clickfctn = function() {
 				
 	var that = this;
-	d3.select(this)
-			.attr("stroke", selectColor);
-			
-	node_id = that.__data__[5];
-	highlightEllipse(node_id);
-	highlightRect(node_id);
-	
+
 	// Only change projection basis if pressing alt
 	if (d3.event && d3.event.altKey) {
+		node_id = that.__data__[5];
 		basis_id = node_id;
+		highlightBasisRect(basis_id);
 		getReprojectedEllipsesFromServer();
+	}
+	
+	else {
+		d3.select(this)
+				.attr("stroke", selectColor);
+			
+		node_id = that.__data__[5];
+		highlightSelectedEllipse(node_id);
+		highlightSelectedRect(node_id);
 	}
 };
 
@@ -219,8 +233,9 @@ var clickfctn = function() {
 var dblclickfctn = function() {
 				
 	basis_id = 0;
-// 	highlightEllipse(0);
-// 	highlightRect(0);
+// 	highlightSelectedEllipse(0);
+// 	highlightSelectedRect(0);
+	highlightBasisRect(0);
 	getReprojectedEllipsesFromServer();
 };
 
@@ -239,7 +254,7 @@ var zoomIcicleView = function(sel_id) {
 			.attr("height", function(d) { return y_ice(d.y + d.dy) - y_ice(d.y); });
 };
 
-var highlightEllipse = function(sel_id) {
+var highlightSelectedEllipse = function(sel_id) {
 
 	// Unhighlight previously selected ellipse
 	d3.select(".el_selected")
@@ -254,7 +269,7 @@ var highlightEllipse = function(sel_id) {
 		.classed("el_selected", true);
 };
 
-var highlightRect = function(sel_id) {
+var highlightSelectedRect = function(sel_id) {
 
 	// Unhighlight previously selected icicle rectangle
 	d3.select(".r_selected")
@@ -265,35 +280,55 @@ var highlightRect = function(sel_id) {
 		.classed('r_selected', true);			
 };
 
+var highlightBasisRect = function(sel_id) {
+
+	// Unhighlight previously selected icicle rectangle
+	d3.select(".r_basis")
+		.classed("r_basis", false);
+
+	// Highlight currently clicked rectangle
+	d3.select("#r_" + sel_id)
+		.classed('r_basis', true);			
+};
+
 var rect_click = function(d) {
 	
 	// NOTE: If click on a different scale rectangle while scatterplot is
 	//   projected into a non-scale-0 basis, change to that different scale
 	//   representation in the scatterplot, but basis projected into is now not
 	//   one that is present in the ellipses on the plot...
-	
-	// TODO: Make separate colors for highlight and projection basis...
-	
+		
 	// Only update icicle zoom if alt has been selected
-	if (d3.event && d3.event.altKey) {
+	if (d3.event && d3.event.shiftKey) {
 		x_ice.domain([d.x, d.x + d.dx]);
 		y_ice.domain([d.y, 1]).range([d.y ? 20 : 0, h_ice]);
 		
 		zoomIcicleView(d.i);
 	}
 	
+	// Only change projection basis if pressing alt
+	else if (d3.event && d3.event.altKey) {
+		basis_id = d.i;
+		highlightBasisRect(basis_id);
+		getReprojectedEllipsesFromServer();
+	}
+
 	// Only transition ellipses if new rect has been selected
-	if (node_id != d.i) {
+	else if (node_id != d.i) {
 		var new_scale = scales_by_id[node_id] == scales_by_id[d.i] ? false : true;
 		node_id = d.i;
-		highlightRect(node_id);
+		highlightSelectedRect(node_id);
 		
 		// Making a copy of the array with concat(), slice() would also work...
 		if (new_scale) {
 			visible_ellipse_ids = [0].concat(ids_by_scale[scales_by_id[node_id]]);
 			updateEllipses();
 		}
-		highlightEllipse(node_id);
+		highlightSelectedEllipse(node_id);
+	}
+	
+	else {
+		return;
 	}
 };
 
@@ -333,6 +368,7 @@ function brushmove(p) {
 	// Saving all scale nodes and 0 from selection mode
 	background_ellipse_ids = [0].concat(ids_by_scale[scales_by_id[node_id]]);
 	visible_ellipse_ids = [];
+	var idx = -1;
 	
 	d3.selectAll("rect").classed("hidden", function(d) {
 		dd = [[d.x, d.y], [d.x + d.dx, d.y + d.dy]];
@@ -340,14 +376,19 @@ function brushmove(p) {
 		// Update global visible_ellipse_ids array
 		// NOTE: There are some rect elements that aren't tree nodes, trying to filter...
 		if(!no_overlap && d.hasOwnProperty('i')) {
-			// Make sure item not already in list from selection or 0 node
+			// Make sure item not already in list from selection or 0 node 
 			if (visible_ellipse_ids.indexOf(d.i) < 0) {
 				visible_ellipse_ids.push(d.i);
+				// If already in background list, remove so will be visible for sure
+				idx = background_ellipse_ids.indexOf(d.i);
+				if (idx >= 0) {
+					background_ellipse_ids.splice(idx,1);
+				}
 			}
 		}
 		return no_overlap;
 	});
-	updateEllipses();
+	updateEllipses(1);
 }
 
 // If the brush is empty, select all circles.
@@ -394,7 +435,7 @@ var init_icicle_view = function() {
 				.data(ice_partition)
 			.enter().append("svg:rect")
 				.attr("id", function(d) {return "r_" + d.i;})
-				.classed("r_selected", function(d){return d.i == node_id;})
+				// .classed("r_selected", function(d){return d.i == node_id;})
 				.attr("x", function(d) { return x_ice(d.x); })
 				.attr("y", function(d) { return y_ice(d.y); })
 				.attr("width", function(d) { return x_ice(d.dx); })
@@ -424,7 +465,7 @@ var init_icicle_view = function() {
 						setIceInstructionsToSelect();
 						background_ellipse_ids = [];
 						visible_ellipse_ids = [0].concat(ids_by_scale[scales_by_id[node_id]]);
-						updateEllipses();
+						updateEllipses(100);
 						brush_on = false;
 					}
 					else {
@@ -436,9 +477,12 @@ var init_icicle_view = function() {
 							.call(brush);
 						setIceInstructionsToBrush();
 						brush_on = true;
+						brushmove();
 					}
 				}
 			});
+		// NOTE: hard-coded initial projection basis node 0
+		highlightBasisRect(0);
 	});
 };
 
