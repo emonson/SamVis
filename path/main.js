874ef23bf360aa458ab4d64088352e4ef54abc97
Eric E Monson <emonson@cs.duke.edu>
2013-08-23 16:45:26 -0400

    trying to add dragslider, but do not have it wired up yet

--- path/main.js
+++ path/main.js
@@ -2,6 +2,21 @@
 // MAIN
 
 window.onload = function() {
+	
+
+$(function(){
+
+	// Slider
+	$('#slider').dragslider({
+		animate: true,
+		range: true,
+		rangeDrag: true,
+		values: [30, 70],
+		slide: function( event, ui ) {
+					$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+				}  
+	});
+});
 
 	$.subscribe("/district/ellipse_click", DISTRICT.visgen);
 	$.subscribe("/district/ellipse_hover", DISTRICT.el_hover);
