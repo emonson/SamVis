b6188fa8ae8212d0b6bf10e1fac11d6aaa941786
Eric E Monson <emonson@cs.duke.edu>
2014-02-28 10:15:32 -0500

    got path transitions working in funciton plot

--- path/centerfunc.js
+++ path/centerfunc.js
@@ -8,76 +8,97 @@ var CENTER_VIS = (function(d3, $, g){
 	// Append vis-specific div
 	d3.select("#district_center_data_vis").append('div').attr('id', 'district_center_func');
 
-	var margin = {top: 10, right: 10, bottom: 20, left: 30},
-			width = 200 - margin.left - margin.right,
-			height = 150 - margin.top - margin.bottom;
+	// Keep around a consistent place to store the line plot data
+	// and fill with dummy data
+  var data = [];
+  for (var ii = 0; ii < 100; ii++) {
+  	data.push(0);
+  }
 
-	var x = d3.scale.linear()
-			.range([0, width]);
+  var margin = {top: 6, right: 0, bottom: 6, left: 6},
+      width = 180 - margin.left - margin.right,
+      height = 120 - margin.top - margin.bottom;
 
-	var y = d3.scale.linear()
-			.range([height, 0]);
+  var x = d3.scale.linear()
+      .domain([0, data.length-1])
+      .range([0, width]);
 
-	var xAxis = d3.svg.axis()
-			.scale(x)
-			.orient("bottom")
-			.ticks(3);
+  var y = d3.scale.linear()
+      .domain([-1, 1])
+      .range([height, 0]);
 
 	var yAxis = d3.svg.axis()
 			.scale(y)
 			.orient("left")
 			.ticks(0);
 
-	var line = d3.svg.line()
-			.x(function(d,i) { return x(i); })
-			.y(function(d) { return y(d); });
+  var line = d3.svg.line()
+      .x(function(d, i) { return x(i); })
+      .y(function(d, i) { return y(d); });
 
-	var svg = d3.select("#district_center_func").append("svg")
-			.attr("width", width + margin.left + margin.right)
-			.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var svg = d3.select("#district_center_func").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	// Since we're using the whole center data set bounds, shouldn't have to update axes
-	svg.append("g")
-			.attr("class", "x axis")
-			.attr("transform", "translate(0," + height + ")")
-			.call(xAxis);
+  svg.append("defs").append("clipPath")
+      .attr("id", "clip")
+    .append("rect")
+      .attr("width", width)
+      .attr("height", height);
 
-	svg.append("g")
-			.attr("class", "y axis")
-			.call(yAxis);
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
 
+  var path = svg.append("g")
+      .attr("clip-path", "url(#clip)")
+    .append("path")
+      .data([data])
+      .attr("class", "plotline")
+      .attr("d", line);
+
+
+	var updatePlot = function(delay) {
+		
+	 delay = delay || 100;
+	 
+	 // transition the line
+		path.data([data])
+			.transition()
+				.duration(delay)
+				.attr("d", line)
+		
+		svg.selectAll("g.y.axis").call(yAxis);
+		
+	};
+
+	// PUBLIC methods
+	
 	// Get basis images from server
 	ci.getCenterDataFromServer = function(id) {
 
 		d3.json(g.data_proxy_root + '/' + g.dataset + "/districtcenterdata?district_id=" + id, function(json) {
 	
+			var orig_n = data.length;
+			
 			g.district_center_data = json.data;
 			g.district_center_data_dims = json.data_dims;
 			g.district_center_range = json.data_range;
 
-			x.domain([0, g.district_center_data[0].length]);
+			// Assign the plot data
+			data = g.district_center_data[0];
+			x.domain([0, data.length-1]);
 			y.domain(g.data_bounds);
-
-			updatePlot();
+			
+			// First transition fast
+			if (orig_n == 2) {
+				updatePlot(0);
+			} else {
+				updatePlot(1000);
+			}
 		});
-	};
-
-	var updatePlot = function() {
-		
-		// TODO: Use transition instead of replacing line each time!
-		svg.selectAll(".line").remove();
-		
-		svg.selectAll("g.y.axis").call(yAxis);
-		svg.selectAll("g.x.axis").call(xAxis);
-		
-		// This is hard-coded to only take one data series in a 2d array right now...
-		svg.append("path")
-				.datum(g.district_center_data[0])
-				.attr("class", "line")
-				.attr("d", line);
-		
 	};
 
 	return ci;
