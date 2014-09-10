75926c3f13c6864734dc74ff1e5cdc298f463b4a
Eric E Monson <emonson@cs.duke.edu>
2014-09-10 15:35:32 -0400

    removing older code

--- X_Older/ellipses.php
+++
@@ -1,5 +0,0 @@
-<?php
-$n_pts = $_GET['n'];
-print_r(file_get_contents("http://localhost:9000/newdata?n=" . $n_pts));
-// print_r(file_get_contents("http://emo2.trinity.duke.edu:9000/newdata?n=" . $n_pts));
-?>
